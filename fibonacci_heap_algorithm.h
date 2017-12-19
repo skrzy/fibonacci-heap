2ef0eed5d61cd89be52e30f9f09445d3ac558b2e
skrzy <strek.krzysztof@gmail.com>
2017-12-19 22:26:48 +0100

    sort alghoritm as template

--- fibonacci_heap_algorithm.h
+++ fibonacci_heap_algorithm.h
@@ -3,8 +3,9 @@
 
 #include "fibonacci_heap.h"
 
-fibonacci_heap<int> make_fibonacci_heap(int* begin, int* end) {
-    fibonacci_heap<int> heap;
+template <typename T>
+fibonacci_heap<T> make_fibonacci_heap(T* begin, T* end) {
+    fibonacci_heap<T> heap;
     while (begin != end) {
         heap.push(*begin);
         begin++;
@@ -12,8 +13,9 @@ fibonacci_heap<int> make_fibonacci_heap(int* begin, int* end) {
     return heap;
 }
 
-int* sort_fibonacci_heap(fibonacci_heap<int>& heap) {
-    int * result = new int[heap.size()];
+template <typename T>
+int* sort_fibonacci_heap(fibonacci_heap<T>& heap) {
+    T * result = new T[heap.size()];
     int index = 0;
 
     while (!heap.empty()) {
